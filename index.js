d1eb43c0d7e5aeedd3304fe739bf73494101dc73
TheRealAyuwoki <132645866+TheRealAyuwoki@users.noreply.github.com>
2025-03-04 02:09:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-var randomNumber_1 = Math.floor(Math.random()*6)+1;
+function tirarDados() {
+    var randomNumber_1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber_2 = Math.floor(Math.random() * 6) + 1;
 
-var dado_1 = "dado" + randomNumber_1 + ".png";
+    var dado_1 = "images/dice" + randomNumber_1 + ".png";
+    var dado_2 = "images/dice" + randomNumber_2 + ".png";
 
-var randomNumber_2 = Math.floor(Math.random()*6)+1;
+    var image_1 = document.querySelectorAll("img")[0];
+    var image_2 = document.querySelectorAll("img")[1];
 
-var dado_2 = "dado" + randomNumber_2 + ".png";
+    image_1.setAttribute("src", dado_1);
+    image_2.setAttribute("src", dado_2);
 
-var randomImageSource_1 = "images/" + dado_1;
-
-var randomImageSource_2 = "images/" + dado_2;
-
-var image_1 = document.querySelectorAll("img")[0];
-
-var image_2 = document.querySelectorAll("img")[1];
-
-image_1.setAttribute("src", randomImageSource_1);
-image_2.setAttribute("src", randomImageSource_2);
-
-if (randomNumber_1 > randomNumber_2) {
-    document.querySelector("h1").textContent = "🚩El Jugador 1 Gana!!";
-} else if (randomNumber_1 < randomNumber_2) {
-    document.querySelector("h1").textContent = "El Jugador 2 Gana!!🚩";
-} else {
-    document.querySelector("h1").textContent = "Tenemos un empate!!";
+    if (randomNumber_1 > randomNumber_2) {
+        document.querySelector("h1").textContent = "🚩 El Jugador 1 Gana!!";
+    } else if (randomNumber_1 < randomNumber_2) {
+        document.querySelector("h1").textContent = "El Jugador 2 Gana!! 🚩";
+    } else {
+        document.querySelector("h1").textContent = "Tenemos un empate!!";
+    }
 }
